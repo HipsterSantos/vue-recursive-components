a989be49feea2d9020cae5aa3ad09440f022a621
santosferreira <santoscampos269@gmail.com>
2022-10-11 22:08:39 +0100

    [add] vue-treeview

---
+++ src/components/Home.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <TreeNode v-for="(node,index) in nodes" :key="index" :node="node"/>
+    Welcome to vue-tree-view by using recursive call 
+  </div>
+</template>
+
+<script>
+import TreeNode from './TreeNode.vue'
+export default {
+  name: 'HomeComponent',
+  components:{
+    'TreeNode':TreeNode
+  },
+  props: {
+    msg: String
+  },
+  computed: {
+    nodes() {
+      return [
+        {
+          id: 1,
+          label: 'Foods',
+          children: [
+            {
+              id: 2,
+              label: 'Fruits',
+              children: [
+                {
+                  id: 3,
+                  label: 'Banana'
+                },
+                {
+                  id: 4,
+                  label: 'Apple'
+                },
+                {
+                  id: 5,
+                  label: 'Strawberry'
+                }
+              ]
+            },
+            {
+              id: 6,
+              label: 'Vegetables',
+              children: [
+                {
+                  id: 7,
+                  label: 'Carrot'
+                },
+                {
+                  id: 8,
+                  label: 'Lettuce'
+                },
+                {
+                  id: 9,
+                  label: 'Potato'
+                }
+              ]
+            }
+          ]
+        },
+        {
+          id: 10,
+          label: 'Drinks',
+          children: [
+            {
+              id: 11,
+              label: 'Beers',
+              children: [
+                {
+                  id: 12,
+                  label: 'Budweiser'
+                },
+                {
+                  id: 13,
+                  label: 'Heineken'
+                }
+              ]
+            },
+            {
+              id: 14,
+              label: 'Wines'
+            },
+            {
+              id: 15,
+              label: 'Whiskey'
+            }
+          ]
+        }
+      ]
+    }
+}//computed property ending 
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/TreeNode.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="node-item">
+        {{node.label}}
+        <div v-if="hasChildren" class="children-node">
+            <TreeNodeComponent v-for="(child) in node.children" :key="child.id" :node="child"/>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'TreeNodeComponent',
+    props:{
+        node:{
+            type: Object,
+            required:true 
+        }
+    },
+    data(){
+        return {}
+    },
+    computed:{
+        hasChildren(){
+            let {children} = this.node
+            return  children && children.length > 0
+        }
+    },
+    mounted(){
+        console.log('treenode component is mounted ')
+    }
+}
+</script>
+
+<style scoped>
+.node-item{
+    padding: 1em;
+    width:200px;
+    background: rgb(80, 80, 186);
+    font-size: 17pt;
+    font-style: normal;
+    border-radius: 1em;
+    color:#fff;
+    font-weight: 700;
+    margin: 1em 0;
+    
+}
+.children-node{
+    background: rgb(51, 119, 40);
+    margin: 1em 0.3em
+}
+</style>
